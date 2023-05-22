6628a6f9c4468e2f7063e4f923aa05aec68cfcec
grig0406 <127596065+grig0406@users.noreply.github.com>
2023-05-22 21:17:50 +0700

    Add files via upload

---
+++ 2023.05.21/1.py
@@ -0,0 +1,7 @@
+name = str(input("Введите имя: "))
+last_name = str(input("Введите фамилию: "))
+year = int(input("Введите год рождения: "))
+
+age = 2023 - year
+
+print(last_name, ' ', name, ',', ' ', age, sep='')
