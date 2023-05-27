efbb08254f4d35fdc9081488eddce9165328a3e4
grig0406 <127596065+grig0406@users.noreply.github.com>
2023-05-27 15:25:03 +0700

    Update 4.py

--- 2023.05.21/4.py
+++ 2023.05.21/4.py
@@ -1,18 +1,20 @@
 number = int(input())
 
 # КОММЕНТАРИЙ: цифра — digit, число — number, num
-first_number = number // 100
-second_number = number % 10
-third_number = (number % 100) // 10
+first_digit = number // 100
+second_digit = number % 10
+third_digit = (number % 100) // 10
 
-sum_numbers = first_number + second_number + third_number
-product_numbers = first_number * second_number * third_number
+sum_numbers = first_digit + second_digit + third_digit
+product_numbers = first_digit * second_digit * third_digit
 
-print(f'Сумма цифр = {sum_numbers}')
-print(f'Произведение цифр = {product_numbers}')
+print(f'Сумма цифр = {sum_numbers}\nПроизведение цифр = {product_numbers}')
+# print(f'Произведение цифр = {product_numbers}')
 
 
 # ДОБАВИТЬ: результат выполнения программы с собственными данными — в закомментированном виде
-
+# 333
+# Сумма цифр = 9
+# Произведение цифр = 27
 
 # ИТОГ: хорошо, но нужно лучше — 1/3
