dfe04212c44deba12c799be00a02a9241344b907
Gennadiy <renardes@gmail.com>
2023-05-27 11:48:08 +0500

    2023.05.21: комментарии, правки, итоги

--- 2023.05.21/4.py
+++ 2023.05.21/4.py
@@ -1,13 +1,18 @@
 number = int(input())
 
+# КОММЕНТАРИЙ: цифра — digit, число — number, num
 first_number = number // 100
 second_number = number % 10
 third_number = (number % 100) // 10
 
 sum_numbers = first_number + second_number + third_number
-
 product_numbers = first_number * second_number * third_number
 
 print(f'Сумма цифр = {sum_numbers}')
-
 print(f'Произведение цифр = {product_numbers}')
+
+
+# ДОБАВИТЬ: результат выполнения программы с собственными данными — в закомментированном виде
+
+
+# ИТОГ: хорошо, но нужно лучше — 1/3
