8fd647a55c562319a56126b56811f7e88413dc37
grig0406 <127596065+grig0406@users.noreply.github.com>
2023-06-01 23:17:25 +0700

    Add files via upload

---
+++ 2023.05.28/1.py
@@ -0,0 +1,27 @@
+first_num = float(input())
+second_num = float(input())
+third_num = float(input())
+
+if first_num > 0:
+    one = first_num + 0
+else:
+    one = first_num * 0
+
+if second_num > 0:
+    tow = second_num + 0
+else:
+    tow = second_num * 0
+
+if third_num > 0:
+    three = third_num + 0
+else:
+    three = third_num * 0
+
+print(one + tow + three)
+
+# ввод:
+# 4
+# -22
+# 1.5
+# вывод:
+# 5.5
