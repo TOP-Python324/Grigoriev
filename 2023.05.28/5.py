5c749fa852bdf035235b3e0224e361937d2ab65a
grig0406 <127596065+grig0406@users.noreply.github.com>
2023-06-03 12:40:26 +0700

    Add files via upload

---
+++ 2023.05.28/5.py
@@ -0,0 +1,22 @@
+one = str(input())
+tow = str(input())
+
+letters = ['a','b','c','d','e','f','g','h']
+numbers = ['1','2','3','4','5','6','7','8']
+
+if one [0] == tow [0] or one [1] == tow [1]:
+    print('Да')
+else:
+    print('Нет')
+
+# Ввод:
+# a1
+# a4
+# Вывод:
+# Да
+
+# Ввод:
+# a1
+# b2
+# Вывод:
+# Нет
