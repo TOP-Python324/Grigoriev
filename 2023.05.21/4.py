6d6b1dc541ce39b4efd636ff52c86e9ac7e272e0
Gennadiy <renardes@gmail.com>
2023-06-04 10:11:48 +0500

    2023.05.21: доработка 1

--- 2023.05.21/4.py
+++ 2023.05.21/4.py
@@ -1,20 +1,20 @@
 number = int(input())
 
-# КОММЕНТАРИЙ: цифра — digit, число — number, num
 first_digit = number // 100
 second_digit = number % 10
 third_digit = (number % 100) // 10
 
+# УДАЛИТЬ: данные переменные используются каждая только единожды — оптимизируйте
 sum_numbers = first_digit + second_digit + third_digit
 product_numbers = first_digit * second_digit * third_digit
 
-print(f'Сумма цифр = {sum_numbers}\nПроизведение цифр = {product_numbers}')
-# print(f'Произведение цифр = {product_numbers}')
+print(f'Сумма цифр = {sum_numbers}\n'
+      f'Произведение цифр = {product_numbers}')
 
 
-# ДОБАВИТЬ: результат выполнения программы с собственными данными — в закомментированном виде
 # 333
 # Сумма цифр = 9
 # Произведение цифр = 27
 
-# ИТОГ: хорошо, но нужно лучше — 1/3
+
+# ИТОГ: хорошо, ещё немного доработать — 2/3
